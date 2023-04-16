b6421aa82880723eeba72d6f6bef716ef875cc5a
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-16 21:27:55 +0200

    fix location not updating

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import { BiCameraMovie, BiSlideshow } from 'react-icons/bi';
 import { MdFilterList, MdOutlineHome } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Navigation.scss';
 
@@ -10,25 +10,25 @@ const navItems = [
   { content: 'Series', href: '/show', icon: <BiSlideshow /> },
   { content: 'Filters', href: '/filter', icon: <MdFilterList /> },
 ];
-
-const Navigation = ({ classNames }) => (
-  <nav className={`navigation ${classNames}`}>
-    <ul className="navigation__list">
-      {navItems?.map((navItem) => (
-        <li
-          key={navItem.content}
-          className={`navigation__item ${
-            window.location.pathname === navItem.href ? 'active' : ''
-          }`}
-        >
-          <Link to={navItem.href}>
-            {navItem.icon}
-            {navItem.content}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Navigation = ({ classNames }) => {
+  const location = useLocation();
+  return (
+    <nav className={`navigation ${classNames}`}>
+      <ul className="navigation__list">
+        {navItems?.map((navItem) => (
+          <li
+            key={navItem.content}
+            className={`navigation__item ${location.pathname === navItem.href ? 'active' : ''}`}
+          >
+            <Link to={navItem.href}>
+              {navItem.icon}
+              {navItem.content}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
