fded64bb4d9c9f759013516a195d982d423321ee
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-17 14:21:21 +0200

    fix index as key

--- src/components/Carousel/Indicators/Indicators.jsx
+++ src/components/Carousel/Indicators/Indicators.jsx
@@ -1,17 +1,24 @@
 /* eslint-disable react/no-array-index-key */
+import { Children } from 'react';
+
 import styles from './Indicators.module.scss';
 
-const Indicators = ({ childrensCount, itemsShow, page, updateActive }) => (
-  <div className={styles.indicators}>
-    {new Array(Math.ceil(childrensCount / itemsShow)).fill('').map((_, index) => (
-      <button
-        key={index}
-        type="button"
-        onClick={() => updateActive(index)}
-        className={`${styles.indicator} ${index === page ? styles.active : ''}`}
-      />
-    ))}
-  </div>
-);
+const Indicators = ({ childrensCount, itemsShow, page, updateActive }) => {
+  const items = new Array(Math.ceil(childrensCount / itemsShow)).fill('');
+
+  return (
+    <div className={styles.indicators}>
+      {Children.toArray(
+        items?.map((_, index) => (
+          <button
+            type="button"
+            onClick={() => updateActive(index)}
+            className={`${styles.indicator} ${index === page ? styles.active : ''}`}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
 export default Indicators;
