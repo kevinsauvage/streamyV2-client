1ca2115c59a649f900b427207a0f8f0e4f84ce68
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-17 12:40:55 +0200

    make use of object destructuring

--- src/components/Acordeon/AcordeonCard/AcordeonCard.jsx
+++ src/components/Acordeon/AcordeonCard/AcordeonCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { Link } from 'react-router-dom';
 
 import { image } from '../../../helpers/requests';
@@ -5,34 +6,36 @@ import CardBtns from '../../CardBtns/CardBtns';
 
 import './AcordeonCard.scss';
 
-const AcordeonCard = ({ movie, active, activeLeft, activeRight, type }) => (
-  <div
-    className={`AcordeonCard ${active && 'AcordeonCard__active'} ${
-      activeLeft && 'AcordeonCard__active--side AcordeonCard__active--left'
-    } ${activeRight && 'AcordeonCard__active--side AcordeonCard__active--right'}`}
-  >
-    <img
-      className="AcordeonCard__img"
-      src={
-        window.innerWidth < 400
-          ? `${image.url.w500}${movie?.poster_path}`
-          : `${image.url.w780}${movie?.poster_path}`
-      }
-      alt={movie?.name || movie?.original_title || movie?.original_name}
-      width="500"
-      height="750"
-      loading="lazy"
-    />
-    <div className="AcordeonCard__detail">
-      <Link to={`/${type || movie?.media_type}/${movie.id}`}>
-        <h6 className="AcordeonCard__title">
-          {movie?.title || movie?.original_title || movie?.original_name}
-        </h6>
-      </Link>
-      <p className="AcordeonCard__overview">{movie?.overview}</p>
-      <CardBtns movie={movie} type={type} />
+const AcordeonCard = ({ movie, active, activeLeft, activeRight, type }) => {
+  const { name, poster_path, original_title, original_name, media_type, id, overview, title } =
+    movie || {};
+  return (
+    <div
+      className={`AcordeonCard ${active && 'AcordeonCard__active'} ${
+        activeLeft && 'AcordeonCard__active--side AcordeonCard__active--left'
+      } ${activeRight && 'AcordeonCard__active--side AcordeonCard__active--right'}`}
+    >
+      <img
+        className="AcordeonCard__img"
+        src={
+          window.innerWidth < 400
+            ? `${image.url.w500}${poster_path}`
+            : `${image.url.w780}${poster_path}`
+        }
+        alt={name || original_title || original_name}
+        width="500"
+        height="750"
+        loading="lazy"
+      />
+      <div className="AcordeonCard__detail">
+        <Link to={`/${type || media_type}/${id}`}>
+          <h6 className="AcordeonCard__title">{title || original_title || original_name}</h6>
+        </Link>
+        <p className="AcordeonCard__overview">{overview}</p>
+        <CardBtns movie={movie} type={type} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AcordeonCard;
