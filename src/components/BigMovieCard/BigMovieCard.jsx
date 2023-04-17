7d754684153d8495db99e49e501e7afc58abab76
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-17 20:45:02 +0200

    improve bannerslider

--- src/components/BigMovieCard/BigMovieCard.jsx
+++ src/components/BigMovieCard/BigMovieCard.jsx
@@ -22,33 +22,36 @@ const BigMovieCard = ({ movie, title, type }) => {
   } = movie || {};
 
   return (
-    <div
-      className={styles.card}
-      style={{
-        backgroundImage: `url('${
-          window.innerWidth < 500 ? image.url.w780 : image.url.w1280
-        }${backdrop_path}')`,
-      }}
-    >
-      <Container>
-        <div className={styles.details}>
-          <p className={styles.title}>{title}</p>
-          <Link to={`/${type || media_type}/${id}`}>
-            <h2 className={styles.name}>{original_title || original_name}</h2>
-          </Link>
-          <div className={styles.row}>
-            <p className={styles.date}>
-              {extractYearFromDate(release_date) || extractYearFromDate(first_air_date)}{' '}
-            </p>
-            <div className={styles.note}>
-              <span>{Math.ceil(vote_average?.toFixed(2))}</span> / 10
+    <div className={styles.card}>
+      <img
+        className={styles.image}
+        src={`${window.innerWidth < 500 ? image.url.w780 : image.url.w1280}${backdrop_path}`}
+        alt={original_name || original_title}
+        width={1280}
+        height={750}
+      />
+
+      <div className={styles.content}>
+        <Container>
+          <div className={styles.details}>
+            <p className={styles.title}>{title}</p>
+            <Link to={`/${type || media_type}/${id}`}>
+              <h2 className={styles.name}>{original_title || original_name}</h2>
+            </Link>
+            <div className={styles.row}>
+              <p className={styles.date}>
+                {extractYearFromDate(release_date) || extractYearFromDate(first_air_date)}{' '}
+              </p>
+              <div className={styles.note}>
+                <span>{Math.ceil(vote_average?.toFixed(2))}</span> / 10
+              </div>
+              <p className={styles.type}>{media_type === 'tv' ? 'show' : media_type}</p>
             </div>
-            <p className={styles.type}>{media_type === 'tv' ? 'show' : media_type}</p>
+            <p className={styles.overview}>{overview}</p>
+            <CardBtns movie={movie} type={media_type === 'tv' ? 'show' : media_type} />
           </div>
-          <p className={styles.overview}>{overview}</p>
-          <CardBtns movie={movie} type={media_type === 'tv' ? 'show' : media_type} />
-        </div>
-      </Container>
+        </Container>
+      </div>
     </div>
   );
 };
