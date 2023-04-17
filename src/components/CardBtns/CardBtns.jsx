0455e284be3c4edfceb232c66685c33737485c8a
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-17 12:44:42 +0200

    switch to css module

--- src/components/CardBtns/CardBtns.jsx
+++ src/components/CardBtns/CardBtns.jsx
@@ -7,24 +7,24 @@ import { Link } from 'react-router-dom';
 
 import { UserContext } from '../../context/UserContext';
 
-import './CardBtns.scss';
+import styles from './CardBtns.module.scss';
 
 const CardBtns = ({ movie, type, className = '' }) => {
   const { addToMovieList, removeFromMovieList, userMovies } = useContext(UserContext);
   const { media_type, id } = movie || {};
 
   return (
-    <div className={`CardBtns ${className}`}>
-      <Link className="CardBtns__btn" to={`/${type || media_type}/${movie?.id}`}>
+    <div className={`${styles.buttons} ${className}`}>
+      <Link className={styles.button} to={`/${type || media_type}/${movie?.id}`}>
         <BsPlayFill /> See details
       </Link>
       {userMovies && movie && userMovies.map((item) => item.movie?.id).includes(id) ? (
-        <button type="button" className="CardBtns__btn" onClick={() => removeFromMovieList(movie)}>
+        <button type="button" className={styles.button} onClick={() => removeFromMovieList(movie)}>
           <MdRemoveCircleOutline />
           watch list
         </button>
       ) : (
-        <button type="button" className="CardBtns__btn" onClick={() => addToMovieList(movie, type)}>
+        <button type="button" className={styles.button} onClick={() => addToMovieList(movie, type)}>
           <IoMdAdd />
           Watch List
         </button>
