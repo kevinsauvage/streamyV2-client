ce938abb2e8044707f3db6393812a2728881afdc
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-17 13:05:05 +0200

    switch to css module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,17 @@ import Navigation from '../Navigation/Navigation';
 import SearchModal from '../SearchModal/SearchModal';
 import UserMenu from '../UserMenu/UserMenu';
 
-import './Header.scss';
+import styles from './Header.module.scss';
 
 const Header = () => {
   const [displaySearch, setDisplaySearch] = useState(false);
   const [displayUserMenu, setDisplayUserMenu] = useState(false);
 
   return (
-    <header className="Header">
+    <header className={styles.header}>
       <Container>
-        <div className="Header__container">
-          <h1 className="Header__logo">Streamy</h1>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>Streamy</h1>
           <Navigation classNames="hide-mobile" />
           <UserMenu
             setDisplayUserMenu={setDisplayUserMenu}
