a6a74c810ba55dde85775f6496c16d6cffb2c2ac
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-17 13:34:12 +0200

    switch to css module

--- src/components/Acordeon/AcordeonCard/AcordeonCard.jsx
+++ src/components/Acordeon/AcordeonCard/AcordeonCard.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 import { image } from '../../../helpers/requests';
 import CardBtns from '../../CardBtns/CardBtns';
 
-import './AcordeonCard.scss';
+import styles from './AcordeonCard.module.scss';
 
 const AcordeonCard = ({ movie, active, activeLeft, activeRight, type }) => {
   const { name, poster_path, original_title, original_name, media_type, id, overview, title } =
     movie || {};
   return (
     <div
-      className={`AcordeonCard ${active && 'AcordeonCard__active'} ${
-        activeLeft && 'AcordeonCard__active--side AcordeonCard__active--left'
-      } ${activeRight && 'AcordeonCard__active--side AcordeonCard__active--right'}`}
+      className={`${styles.card} ${active ? styles.active : ''} ${
+        activeLeft ? styles['active-left'] : ''
+      } ${activeRight ? styles['active-right'] : ''}`}
     >
       <img
-        className="AcordeonCard__img"
+        className={styles.image}
         src={
           window.innerWidth < 400
             ? `${image.url.w500}${poster_path}`
@@ -27,11 +27,11 @@ const AcordeonCard = ({ movie, active, activeLeft, activeRight, type }) => {
         height="750"
         loading="lazy"
       />
-      <div className="AcordeonCard__detail">
+      <div className={styles.details}>
         <Link to={`/${type || media_type}/${id}`}>
-          <h6 className="AcordeonCard__title">{title || original_title || original_name}</h6>
+          <h6 className={styles.title}>{title || original_title || original_name}</h6>
         </Link>
-        <p className="AcordeonCard__overview">{overview}</p>
+        <p className={styles.overview}>{overview}</p>
         <CardBtns movie={movie} type={type} />
       </div>
     </div>
