e338bc628f58b22fe0890e8693ea3e269c0880f0
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-08-14 18:22:04 +0200

    update style

--- src/components/PlayBanner/PlayBanner.jsx
+++ src/components/PlayBanner/PlayBanner.jsx
@@ -1,5 +1,4 @@
 import { image } from '../../helpers/requests';
-import Container from '../Container/Container';
 
 import styles from './PlayBanner.module.scss';
 
@@ -10,16 +9,14 @@ const PlayBanner = ({ movie, playTrailer }) => (
       src={`${image.url.w1280}${movie?.backdrop_path}`}
       alt={movie?.original_title || movie?.original_name}
     />
-    <Container>
-      <div className={styles.button}>
-        <button type="button" onClick={playTrailer}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-            <polygon points="40,30 65,50 40,70" />
-          </svg>
-        </button>
-        <p>Play Trailer</p>
-      </div>
-    </Container>
+    <div className={styles.button}>
+      <button type="button" onClick={playTrailer}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <polygon points="40,30 65,50 40,70" />
+        </svg>
+      </button>
+      <p>Play Trailer</p>
+    </div>
   </div>
 );
 
